893584045da0d4988ae98e757a24218894d1f5ac
Jason Graham <jgraha8@gmail.com>
2019-09-03 23:59:30 -0400

    Updated audit to 2.8.5
    This version includes up-to-date rule files produced by Red Hat which
    conform to various security frameworks (e.g., STIG, NISPOM, etc.). A
    basic rules file is installed by default.
    
    A note in the README has been added regarding python3 as an optional
    dependency.

--- system/audit/doinst.sh
+++ system/audit/doinst.sh
@@ -24,13 +24,17 @@ preserve_perms() {
 
 preserve_perms etc/rc.d/rc.auditd.new
 preserve_perms etc/rc.d/rc.auditd.conf.new
-config etc/audit/rules.d/audit.rules.new
-config etc/audit/auditd.conf.new
-config etc/audisp/audispd.conf.new
-config etc/audisp/zos-remote.conf.new
-config etc/audisp/plugins.d/af_unix.conf.new
-config etc/audisp/plugins.d/au-remote.conf.new
-config etc/audisp/plugins.d/audispd-zos-remote.conf.new
-config etc/audisp/plugins.d/syslog.conf.new
-config etc/audisp/audisp-remote.conf.new
+for f in `find etc/audit/ etc/audisp/ -name '*.conf.new' -o -name '*.rules.new' -type f`
+do
+    config $f
+done
+#config etc/audit/rules.d/audit.rules.new
+#config etc/audit/auditd.conf.new
+#config etc/audisp/audispd.conf.new
+#config etc/audisp/zos-remote.conf.new
+#config etc/audisp/plugins.d/af_unix.conf.new
+#config etc/audisp/plugins.d/au-remote.conf.new
+#config etc/audisp/plugins.d/audispd-zos-remote.conf.new
+#config etc/audisp/plugins.d/syslog.conf.new
+#config etc/audisp/audisp-remote.conf.new
 config etc/libaudit.conf.new
