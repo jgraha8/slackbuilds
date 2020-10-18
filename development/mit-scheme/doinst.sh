1447f48f82de18769deaeaf76f7fc234306d7858
Jason Graham <jgraham@compukix.net>
2020-10-18 02:27:00 -0400

    Added plugins and optiondb files to be configuration .new files

--- development/mit-scheme/doinst.sh
+++ development/mit-scheme/doinst.sh
@@ -1,6 +1,20 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
 info_install() {
   INFO="$1"
   if [ -x /usr/bin/install-info ]; then
     chroot . /usr/bin/install-info --info-dir=/usr/info $INFO 2> /dev/null
   fi
 }
+
